a95f942df9a49fe3ee9332356074295976994e96
jonathan.rezende <jonathan.rezende@horizonte.dev>
2022-08-03 18:36:48 -0300

    gitinicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export default function App(){
+    
+    return (
+        <>
+            <h3>GESTOR DE TAREFAS</h3>
+            <hr />
+            <input type="text" />
+            <div>
+                <button>Adicionar</button>
+                <button>Limpar Tudo</button>
+            </div>
+            <hr />
+            <p>Tarefas:</p>
+        </>
+    )
+}
