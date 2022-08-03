b0d68adc90c28f8a1178f3e150e5a875b67f10f0
jonathan.rezende <jonathan.rezende@horizonte.dev>
2022-08-03 19:50:30 -0300

    adicionandocomandos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 export default function App(){
+
+    // STATE
+    const [listaTarefas, setListaTarefas] = useState(() => { return [] })
+    const [tarefa, setTarefa] = useState(() => { return ''})
     
+    //REF
+    const idTarefa = useRef(0)
+
+    // METODOS
+    function adicionarTarefa(){
+        setListaTarefas(old => { return [...old, {id: idTarefa.current, texto: tarefa}] })
+        idTarefa.current = idTarefa.current + 1
+        idTarefa.current = 0
+    }
+
+    function limparTarefas(){
+        setListaTarefas(old => { return [] })
+    }
+
     return (
         <>
             <h3>GESTOR DE TAREFAS</h3>
             <hr />
-            <input type="text" />
+            <input type="text" value={tarefa} onChange={(evento) => { setTarefa(evento.target.value)}} />
             <div>
-                <button>Adicionar</button>
-                <button>Limpar Tudo</button>
+                <button onClick={adicionarTarefa}>Adicionar</button>
+                <button onClick={limparTarefas}>Limpar Tudo</button>
             </div>
             <hr />
             <p>Tarefas:</p>
+            {listaTarefas.map((t) => {
+                return <p key={t.id}>{t.texto}</p>
+            })}
         </>
     )
 }
