b84105f2eb77e3113d0198bd69ae30c7f1a60e8f
jonathan.rezende <jonathan.rezende@horizonte.dev>
2022-08-03 20:37:32 -0300

    ListaCompleta

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,22 @@ export default function App(){
     function adicionarTarefa(){
         setListaTarefas(old => { return [...old, {id: idTarefa.current, texto: tarefa}] })
         idTarefa.current = idTarefa.current + 1
-        idTarefa.current = 0
+        setTarefa('')
     }
 
     function limparTarefas(){
         setListaTarefas(old => { return [] })
+            idTarefa.current = 0
+    }
+
+    function removerTarefa(id){
+        const tmp = listaTarefas.filter(tarefa => tarefa.id !== id  )
+        setListaTarefas(tmp)
     }
 
     return (
         <>
-            <h3>GESTOR DE TAREFAS</h3>
+            <h3>LISTA DE TAREFAS</h3>
             <hr />
             <input type="text" value={tarefa} onChange={(evento) => { setTarefa(evento.target.value)}} />
             <div>
@@ -30,8 +36,8 @@ export default function App(){
             </div>
             <hr />
             <p>Tarefas:</p>
-            {listaTarefas.map((t) => {
-                return <p key={t.id}>{t.texto}</p>
+            {listaTarefas.map((tarefa) => {
+                return <p key={tarefa.id}>{tarefa.texto} - <span onClick={() => { removerTarefa(tarefa.id)} }>[remover]</span> </p>
             })}
         </>
     )
