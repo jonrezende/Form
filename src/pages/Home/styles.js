77bae4b87f0af050cf658c60ebd42d3df16ef55c
jonathan.rezende <jonathan.rezende@horizonte.dev>
2022-08-05 00:01:52 -0300

    inciandocss

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        width: 100%;
+        height: 50px;
+        margin-top: 20px;
+        text-align: center;
+    }
